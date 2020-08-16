644fcbc3ee3c3d8d3d17a71db16f26489f202ec3
Niki <nikyecky@gmail.com>
2020-08-16 15:18:39 +0200

    update task timer
    - max break allowed 120 seconds (to refocus and because back button was introduced); makes a significant difference
    - make it work with incomplete tasks

--- task_time.py
+++ task_time.py
@@ -1,17 +1,23 @@
 import json
 import sys
 
+max_pause_to_count_as_continuous = 120
+
 with open(sys.argv[1]) as file:
     data = json.load(file)
-    last_timestamp = data[0]["timestamp"]
     sum_continuous = 0
     sum_total = 0
+    last_timestamp = data[0].get("timestamp")
     for datapoint in data:
-        cur_timestamp = datapoint["timestamp"]
+        cur_timestamp = datapoint.get("timestamp")
+        if not cur_timestamp:
+            continue
         delta = cur_timestamp - last_timestamp
-        if delta < 10:
+        if delta < max_pause_to_count_as_continuous:
             sum_continuous += delta
         sum_total += delta
         last_timestamp = cur_timestamp
-    print("Number of continuous time spent:", sum_continuous, "seconds, which is", sum_continuous/60, "minutes or", sum_continuous/3600, "hours")
-    print("Number of total time spent:", sum_total, "seconds, which is", sum_total/60, "minutes or", sum_total/3600, "hours")
+    print("Number of continuous time spent:", sum_continuous, "seconds, which is",
+          sum_continuous/60, "minutes or", sum_continuous/3600, "hours")
+    print("Number of total time spent:", sum_total, "seconds, which is",
+          sum_total/60, "minutes or", sum_total/3600, "hours")
